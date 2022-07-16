344b3511c06de04a7faea69b1df72500d12bbd3d
Dalton Eggleston <egglestondalton@yahoo.com>
2022-07-15 23:20:19 -0700

    feels good

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,21 @@
-console.log('hello')
+function newUser(event){
+    event.preventDefault()
+
+    let newTask = {}
+    
+    for (let i = 0; i < event.target.length - 1; i++){ // event.target.length - 1 is to account for the button that is also in the form 
+        
+        let name = event.target[i].name
+        let value = event.target[i].value
+
+        newTask[name] = value
+        event.target[i].value = '' // Clears the input fields so you can add more (not important if redirecting).
+
+    }
+
+    axios.post('/todos/', newTask)
+    // .then((response => {
+    //     console.log(response)
+    // })
+    // )
+}
--- to_do_app/views.py
+++ to_do_app/views.py
@@ -1,11 +1,28 @@
 import re
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponse
+from django.forms.models import model_to_dict
+import json
+from .models import *
 
 # Create your views here.
 
+@csrf_exempt
 def index(request):
 
-    return render(request, 'to_do_app/index.html')
+    if request.method == 'POST': 
+        body = json.loads(request.body)
+        title = body['title']
+        description = body['description']
+
+        new_task = Todo(title = title, description = description)
+        new_task.save()
+        return JsonResponse(model_to_dict(new_task))
+
+
+    else:
+        return render(request, 'to_do_app/index.html')
 
 def signup_form(request):
     
