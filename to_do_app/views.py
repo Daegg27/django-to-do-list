9f61b3dc80b56fa34ebccdb0f0830bbf7383b7f7
Dalton Eggleston <egglestondalton@yahoo.com>
2022-07-15 22:21:54 -0700

    second commit

--- to_do_app/views.py
+++ to_do_app/views.py
@@ -1,3 +1,13 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+
+    return render(request, 'to_do_app/index.html')
+
+def signup_form(request):
+    
+
+    return render(request, 'to_do_app/signup_form.html')
